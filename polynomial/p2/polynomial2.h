35d14adfd57ff2c1f657e2aa3441e2348d7f9a00
yellowcat9487 <139218125+yellowcat9487@users.noreply.github.com>
2024-02-07 15:52:38 +0800

    Update polynomial2.h

--- polynomial/p2/polynomial2.h
+++ polynomial/p2/polynomial2.h
@@ -1,23 +1,21 @@
-#include <math.h>
-
 class polynomial2 {
  public:
   const int a, b, c;
-  polynomial2(int _a, int _b, int _c);
 
+  polynomial2(int _a, int _b, int _c);
   int getReturnValue(int x);
   int deg_f();
 };
 
-polynomial2::polynomial2(int _a, int _b, int _c): a(_a), b(_b), c(_c) {
-}
+polynomial2::polynomial2(int _a, int _b, int _c): a(_a), b(_b), c(_c) {}
 
 int polynomial2::getReturnValue(int x) {
-  return a*std::pow(x, 2) + b*x + c;
+  return a*x*x + b*x + c;
 }
 
 int polynomial2::deg_f() {
   if(a != 0) return 2;
-  else if(b != 0) return 1;
-  else if(c != 0) return 0;
-  else return -1;
+  if(b != 0) return 1;
+  if(c != 0) return 0;
+  return -1;
+}
